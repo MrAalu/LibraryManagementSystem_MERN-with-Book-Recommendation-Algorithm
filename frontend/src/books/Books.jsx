42a4bfe82ed97c364213d14e65ddb0b83aed4d7b
MrAalu <lulamit16@gmail.com>
2023-05-27 15:45:30 +0545

    Books Page-> Browse Collections Added (CARDS)
    to make all Cards same height .car h-100 works fine,but content looks bad need to fix

--- frontend/src/books/Books.jsx
+++ frontend/src/books/Books.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-// import BooksCarousel from './BooksCarousel'
+import FilterBooks from './FilterBooks'
+
+import bookDataDummy from './dummyBookData'
 
 const API_URL = 'http://localhost:5000/api/v1/books/'
 const Books = () => {
@@ -23,121 +25,99 @@ const Books = () => {
 
   return (
     <div className='container-fluid'>
-      <div className='row'>
+      {/* <div className='row'>
         <div className='col-md-12 p-0'>
-          {/* <BooksCarousel></BooksCarousel> */}
         </div>
-      </div>
+      </div> */}
 
-      <h1 className='h1 mt-3' style={{ textAlign: 'center' }}>
-        Browse Collections
-      </h1>
-
-      <div className='row'>
-        <h2>Filter</h2>
-        {/* Filter Option */}
-        <div className='col-md-2'>
-          <form className='book-filter-form'>
-            <div className='form-group'>
-              <input
-                type='text'
-                className='form-control'
-                placeholder='Search by title . . .'
-              />
-            </div>
-
-            {/* Category Filter */}
-            <div className='form-group'>
-              <select className='form-control' defaultValue='all'>
-                <option value='all' disabled hidden>
-                  Categories
-                </option>
-                <option value='all'>All Categories</option>
-                <option value='fiction'>Fiction</option>
-                <option value='non-fiction'>Non-Fiction</option>
-                <option value='mystery'>Mystery</option>
-              </select>
-            </div>
-
-            {/* Language Filter */}
-            <div className='form-group'>
-              <select className='form-control' defaultValue='all'>
-                <option value='all' disabled hidden>
-                  Language
-                </option>
-                <option value='english'>English</option>
-                <option value='nepali'>Nepali</option>
-              </select>
-            </div>
-
-            {/* Language Filter */}
-            <div className='form-group'>
-              <select className='form-control' defaultValue='all'>
-                <option value='all' disabled hidden>
-                  Author
-                </option>
-                <option value='english'>English</option>
-                <option value='nepali'>Nepali</option>
-              </select>
-            </div>
-
-            {/* Available Filter (stock/outofStock) */}
-            <div className='form-group'>
-              <div className='form-check'>
-                <label htmlFor='showAvailable' className='form-check-label'>
-                  Show Available
-                </label>
-                <input
-                  type='checkbox'
-                  className='form-check-input'
-                  id='showAvailable'
-                  defaultChecked
-                />
-              </div>
-            </div>
-
-            {/* Apply / Clear FILTER properties */}
-            <div className='form-group'>
-              <button type='button' className='btn btn-success'>
-                Apply Filters
-              </button>
-              <button type='button' className='btn btn-danger'>
-                Clear Filters
-              </button>
-            </div>
-          </form>
+      {/* FILTER BOOKS SECTION */}
+      <div className='row mt-3'>
+        <div className='col-md-3 border'>
+          <FilterBooks></FilterBooks>
         </div>
 
-        {/* Book Collection */}
-        {/* <div className='col-md-10'>
+        {/* Books Collections Section */}
+        <div className='col md-9'>
+          <h1 className='h1' style={{ textAlign: 'center' }}>
+            Browse Collections
+          </h1>
           <div className='row'>
-            {bookData.map((book) => {
-              const { _id, title, image, author } = book
+            {bookDataDummy.map((book) => {
+              const { id, name, img, author } = book
               return (
-                <div key={_id} className='col-md-2'>
-                  <div className='card'>
-                    <img
-                      src={image}
-                      className='card-img-top'
-                      alt='Book Cover'
-                    />
+                <div
+                  className='col-xxl-2 col-lg-3 col-md-4 col-sm-4 col-6 gy-3 '
+                  key={id}
+                >
+                  <div className='card h-100'>
+                    <div className='card-img-top'>
+                      <img
+                        style={{
+                          height: '100%',
+                          width: '100%',
+                        }}
+                        className='img-fluid'
+                        src={img}
+                        alt='book image'
+                      />
+                    </div>
                     <div className='card-body'>
-                      <h3 className='card-title'>{title}</h3>
-                      <p className='card-text'>{author}</p>
-                      <button className='btn btn-primary'>Reserve</button>
-                      <button className='btn btn-secondary'>
-                        View Details
-                      </button>
+                      <h5 className='h5 card-title'>{name}</h5>
+                      <p className='p card-text'>{author}</p>
+                      <div className='form-group mb-2 justify-content-center d-flex'>
+                        <button type='button' className='btn btn-primary me-2'>
+                          Buy
+                        </button>
+                        <button
+                          type='button'
+                          className='btn btn-secondary me-2'
+                        >
+                          View
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               )
             })}
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   )
 }
 
 export default Books
+
+// BACKEND Fetch data lai map gareko
+{
+  /* Book Collection */
+}
+{
+  /* <div className='col-md-10'>
+  <div className='row'>
+    {bookData.map((book) => {
+      const { _id, title, image, author } = book
+      return (
+        <div key={_id} className='col-md-2'>
+          <div className='card'>
+            <img
+              src={image}
+              className='card-img-top'
+              alt='Book Cover'
+            />
+            <div className='card-body'>
+              <h3 className='card-title'>{title}</h3>
+              <p className='card-text'>{author}</p>
+              <button className='btn btn-primary'>Reserve</button>
+              <button className='btn btn-secondary'>
+                View Details
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    })}
+  </div>
+</div> */
+}
