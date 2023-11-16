7ee3aedb6a5c6fb02190d471d0ae63656b7ffdf6
MrAalu <lulamit16@gmail.com>
2023-11-16 14:59:34 +0545

    Docker setup Frontend + Bug FIX!
    in 'AdminAPP.jsx' at line 19,import typo bug FIXed, Explicitly defined Server PORT at ViteConfig.js

--- frontend/src/ADMIN/AdminAPP.jsx
+++ frontend/src/ADMIN/AdminAPP.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Container, Row, Col } from 'react-bootstrap'
-import AdminNavbar from './navbar/AdminNavbar'
-import PagenotFound from './404-pageNotFoundADMIN/PagenotFound'
-import AdminHome from './adminHome/AdminHome'
-import ManageBooks from './manageBooks/ManageBooks'
-import ViewUsers from './viewUsers/ViewUsers'
-import IssuedBooks from './issuedBooks/IssuedBooks'
-import BooksRequests from './booksRequests/BooksRequests'
-import ReturnedBooks from './returnedBooks/ReturnedBooks'
-import EditBookForm from './manageBooks/EditBookForm'
-import AddNewBook from './addNewBook/AddNewBook'
-import AdminLogout from './adminLogout/AdminLogout'
-import UserIndividualPage from './viewUsers/UserIndividualPage'
-import IssueBookToUser from './issuedBooks/IssueBookToUser'
-import Sidebar from './sidebar/Sidebar'
-import AdminSignup from './createAdminAccount/AdminSignup'
-import AdminOtpForm from './adminOTP/adminOtpForm'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import AdminNavbar from "./navbar/AdminNavbar";
+import PagenotFound from "./404-pageNotFoundADMIN/PagenotFound";
+import AdminHome from "./adminHome/AdminHome";
+import ManageBooks from "./manageBooks/ManageBooks";
+import ViewUsers from "./viewUsers/ViewUsers";
+import IssuedBooks from "./issuedBooks/IssuedBooks";
+import BooksRequests from "./booksRequests/BooksRequests";
+import ReturnedBooks from "./returnedBooks/ReturnedBooks";
+import EditBookForm from "./manageBooks/EditBookForm";
+import AddNewBook from "./addNewBook/AddNewBook";
+import AdminLogout from "./adminLogout/AdminLogout";
+import UserIndividualPage from "./viewUsers/UserIndividualPage";
+import IssueBookToUser from "./issuedBooks/IssueBookToUser";
+import Sidebar from "./sidebar/Sidebar";
+import AdminSignup from "./createAdminAccount/AdminSignup";
+// import AdminOtpForm from "./adminOTP/adminOtpForm";
+import AdminOtpForm from "./adminOTP/AdminOtpForm";
 
 const AdminAPP = () => {
   return (
@@ -28,44 +29,44 @@ const AdminAPP = () => {
             <Col md={2}>
               <Sidebar />
             </Col>
-            <Col md={10} className='mt-5'>
+            <Col md={10} className="mt-5">
               <Routes>
-                <Route path='/admin' element={<AdminHome />} />
-                <Route path='/admin/logout' element={<AdminLogout />} />
-                <Route path='/admin/managebooks' element={<ManageBooks />} />
-                <Route path='/admin/viewusers' element={<ViewUsers />} />
+                <Route path="/admin" element={<AdminHome />} />
+                <Route path="/admin/logout" element={<AdminLogout />} />
+                <Route path="/admin/managebooks" element={<ManageBooks />} />
+                <Route path="/admin/viewusers" element={<ViewUsers />} />
                 <Route
-                  path='/admin/viewusers/:id'
+                  path="/admin/viewusers/:id"
                   element={<UserIndividualPage />}
                 />
-                <Route path='/admin/issuedbooks' element={<IssuedBooks />} />
+                <Route path="/admin/issuedbooks" element={<IssuedBooks />} />
                 <Route
-                  path='/admin/issuebooktouser'
+                  path="/admin/issuebooktouser"
                   element={<IssueBookToUser />}
                 />
                 <Route
-                  path='/admin/booksrequests'
+                  path="/admin/booksrequests"
                   element={<BooksRequests />}
                 />
                 <Route
-                  path='/admin/returnedbooks'
+                  path="/admin/returnedbooks"
                   element={<ReturnedBooks />}
                 />
-                <Route path='/admin/addnewbook' element={<AddNewBook />} />
+                <Route path="/admin/addnewbook" element={<AddNewBook />} />
                 <Route
-                  path='/admin/managebooks/:id'
+                  path="/admin/managebooks/:id"
                   element={<EditBookForm />}
                 />
-                <Route path='*' element={<PagenotFound />} />
-                <Route path='/admin/adminsignup' element={<AdminSignup />} />
-                <Route path='/admin/otp' element={<AdminOtpForm />} />
+                <Route path="*" element={<PagenotFound />} />
+                <Route path="/admin/adminsignup" element={<AdminSignup />} />
+                <Route path="/admin/otp" element={<AdminOtpForm />} />
               </Routes>
             </Col>
           </Row>
         </Container>
       </Router>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default AdminAPP
+export default AdminAPP;
